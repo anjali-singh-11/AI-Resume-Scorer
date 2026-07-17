8f31ffdd348fa17be1959a24e4fc0081a38fbc51
anjali-singh-11 <anjali4628@gmail.com>
2026-07-17 22:25:09 +0530

    Build SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import SideBar from './component/Sidebar/sideBar'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import './App.css';
+import SideBar from './component/SideBar/SideBar.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
--- src/component/Sidebar/sideBar.jsx
+++ src/component/Sidebar/sideBar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import styles from './SideBar.module.css'
+import styles from './SideBar.module.css';
+import ArticleIcon from '@mui/icons-material/Article';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
 const SideBar = () => {
   return (
-    <div className={styles.sidebar}>
-        <div className={styles.sidebarIcon}>
-            <div>Resume Screening</div>
+    <div className={styles.sideBar}>
+        <div className={styles.sideBarIcon}>
+            <ArticleIcon sx={{fontSize:54, marginBottom:2}} />
+            <div className={styles.sideBarTopContent}>Resume Screening</div>
+        </div>
+
+        <div className='styles.sideBarOptionsBlock'>
+            <div className='styles.sideBarOption'>
+                <DashboardIcon />
+                <div>Dashboard</div>
+            </div>
         </div>
     </div>
   )
