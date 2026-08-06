0ff52ae76e2a4bb1aeb2a6036edd921d16d4ffa4
anjali-singh-11 <anjali4628@gmail.com>
2026-08-06 12:46:15 +0530

    building admin

--- src/component/Sidebar/sideBar.jsx
+++ src/component/Sidebar/sideBar.jsx
@@ -5,44 +5,49 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../utils/AuthContext';
 
 const SideBar = () => {
 
     const location = useLocation();
-    console.log(location);
-  return (
-    <div className={styles.sideBar}>
-        <div className={styles.sideBarIcon}>
-            <ArticleIcon sx={{fontSize:54, marginBottom:2}} />
-            <div className={styles.sideBarTopContent}>Resume Screening</div>
-        </div>
+    const navigate = useNavigate();
 
-        <div className='styles.sideBarOptionsBlock'>
+    const { isLogin, setLogin, userInfo, setUserInfo } = useContext(AuthContext);
 
-            <Link to={'/dashboard'} className={[styles.sideBarOption,location.pathname === '/dashboard'?styles.selectedOption:null].join(' ')}>
-                <DashboardIcon sx={{fontSize:22}}/>
-                <div>Dashboard</div>
-            </Link>
+    const handleLogout = () => {
+        {/* Please watch the video for ful source code */ }
 
-            <Link to={'history'} className={[styles.sideBarOption,location.pathname === '/history'?styles.selectedOption:null].join(' ')}>
-                <ManageSearchIcon sx={{fontSize:22}}/>
-                <div>History</div>
-            </Link>
-
-            <Link to={'admin'} className={[styles.sideBarOption,location.pathname === '/admin'?styles.selectedOption:null].join(' ')}>
-                <AdminPanelSettingsIcon sx={{fontSize:22}}/>
-                <div>Admin</div>
-            </Link>
-
-            <div className='styles.sideBarOption'>
-                <LogoutIcon sx={{fontSize:22}}/>
-                <div>LogOut</div>
+    }
+    return (
+        <div className={styles.sideBar}>
+            <div className={styles.sideBarIcon}>
+                <ArticleIcon sx={{ fontSize: 54, marginBottom: 2 }} />
+                <div className={styles.sideBarTopContent}>Resume Screening</div>
             </div>
 
+            <div className={styles.sideBarOptionsBlock}>
+
+                {/* Please watch the video for ful source code */}
+
+
+                <Link to={'/history'} className={[styles.sideBarOption, location.pathname === '/history' ? styles.selectedOption : null].join(' ')}>
+                    <ManageSearchIcon sx={{ fontSize: 22 }} />
+                    <div>History</div>
+                </Link>
+                {
+                    {/* Please watch the video for ful source code */ }
+
+                }
+                <div onClick={handleLogout} className={styles.sideBarOption}>
+                    <LogoutIcon sx={{ fontSize: 22 }} />
+                    <div>LogOut</div>
+                </div>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default SideBar
